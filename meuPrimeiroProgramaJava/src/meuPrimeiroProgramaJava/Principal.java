304f7e86b8487cf539260a88f7b46bb6c8f71a71
milena2612 <61795033+milena2612@users.noreply.github.com>
2021-02-12 18:59:08 -0300

    Update Principal.java
    nota

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -12,24 +12,18 @@ import java.util.Scanner;
  */
 public class Principal {
 	public static void main(String[] args) {
-		// Saldação e pergunta o nome.
-		System.out.println("Olá amigo!\nQual seu nome?");
+		// SaldaÃ§Ã£o e pergunta o nome.
+		System.out.println("OlÃ¡ amigo!\nQual seu nome?");
 		
 		// Ler o nome.
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		
-		// Saldação específica para o nome lido. 
-		System.out.printf("Olá %s", nome);
-		
-		// Perguntar a idade
-		System.out.printf("%s, qual � a sua idade?", nome);
-		
-		// Ler idade
-		Integer idade = in.nextInt();
-		
-		System.out.printf("%d! Puxa! Como voc� � jovem!", idade);
+		// SaldaÃ§Ã£o especÃ­fica para o nome lido. 
+		System.out.printf("OlÃ¡ %s", nome);
 		in.close();
+		
+		// Nota: perguntar a idade e exibi-la.
 	}
 
 }
