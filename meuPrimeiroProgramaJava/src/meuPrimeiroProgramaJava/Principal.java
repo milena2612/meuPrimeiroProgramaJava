15648fa79835eaab47cb424f4795c653d6ed192a
milen <milen@LAPTOP-8ECRT3DO>
2021-02-12 17:36:08 -0300

    Alterações nos Comentários.

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -10,19 +10,16 @@ import java.util.Scanner;
 /**
  * @author milena data: 02/02/2021
  */
-public class Principal {// Classe que contém o método principal.
-	/**
-	 * @param args
-	 */
+public class Principal {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Saldação e pergunta o nome.
 		System.out.println("Olá amigo!\nQual seu nome?");
 		
-		// Ler uma string do console usando somente a biblioteca Scanner.
+		// Ler o nome.
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		
-		//Exibir uma string 
+		// Saldação específica. 
 		System.out.printf("Olá %s", nome);
 		in.close();
 				
