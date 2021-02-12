a2792eeceb8b03afb6807f3f564bce174b4331f5
milen <milen@LAPTOP-8ECRT3DO>
2021-02-12 18:57:08 -0300

    Leitura da idade.

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -21,9 +21,15 @@ public class Principal {
 		
 		// Salda√ß√£o espec√≠fica para o nome lido. 
 		System.out.printf("Ol√° %s", nome);
+		
+		// Perguntar a idade
+		System.out.printf("%s, qual È a sua idade?", nome);
+		
+		// Ler idade
+		Integer idade = in.nextInt();
+		
+		System.out.printf("%d! Puxa! Como vocÍ È jovem!", idade);
 		in.close();
-		
-		
 	}
 
 }
